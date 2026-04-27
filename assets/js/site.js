779921d1f01ad770d011e2a8a64cc6f6a8d52b9a
oliv8093 <oliv8093@elev.techcollege.dk>
2026-04-27 12:49:56 +0200

    opgave done

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,20 @@
-console.log("Check check mic check")
+let navne = [
+  "Atal",
+  "Jonas",
+  "Kasper",
+  "Kristian",
+  "Lui",
+  "Mikkel",
+  "Mingus",
+  "Nikolaj",
+  "Oliver",
+  "Tobias",
+  "Vitalii",
+];
+
+function visNavn() {
+    let index = document.getElementById("nameInput").value;
+    let navn = navne [index];
+
+    document.getElementById("resultat").innerHTML = navn;
+}
